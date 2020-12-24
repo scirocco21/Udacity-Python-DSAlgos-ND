5a9546107d7a26ea89c09d96061be1bed28f7609
scirocco21 <sebgertz@gmail.com>
2020-12-24 00:00:23 -0500

    add linked_list class w/ append + to list methods

---
+++ data-structures/linked_list.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+        
+        # Move to the tail (the last node)
+        node = self.head
+        while node.next:
+            node = node.next
+        
+        node.next = Node(value)
+        return
+    
+    def to_list(self):
+        node = self.head
+        result = []
+        while node:
+            result.append(node.value)
+            node = node.next
+        return result
