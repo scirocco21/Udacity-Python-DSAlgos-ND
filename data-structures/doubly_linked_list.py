fb2315f290ec7ab93c53dd9bd5b3b2a5bcca78fa
scirocco21 <sebgertz@gmail.com>
2020-12-24 22:05:26 -0500

    add doubly linked list problem

---
+++ data-structures/doubly_linked_list.py
@@ -0,0 +1,2 @@
+### Exercise: Implement a doubly linked list that can append to the tail in constant time. Make sure to include forward and backward connections when adding a new node to the list.
+
