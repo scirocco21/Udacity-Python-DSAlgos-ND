75c346cc020315c5eb04dd9ce643ffa5fb6c2cb5
scirocco21 <sebgertz@gmail.com>
2020-12-26 01:15:05 -0500

    add solution to doubly linked list

--- data-structures/doubly_linked_list.py
+++ data-structures/doubly_linked_list.py
@@ -1,2 +1,25 @@
 ### Exercise: Implement a doubly linked list that can append to the tail in constant time. Make sure to include forward and backward connections when adding a new node to the list.
-
+class DoubleNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.previous = None
+        
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def append(self, value):
+        # if there's no node, set head and tail values
+        if self.head = None:
+            self.head = Node(value)
+            self.tail = self.head
+            return
+        # the new node is the one after the (old) tail
+        self.tail.next = DoubleNode(value)
+        # the new node's previous node is the (old) tail
+        self.tail.next.previous = self.tail
+        # finally update the tail property
+        self.tail = self.tail.next
+        
