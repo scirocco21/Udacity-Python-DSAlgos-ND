6cf365efa8e8f5b5ca4634f1138280dd91a69508
scirocco21 <sebgertz@gmail.com>
2020-12-27 18:06:03 -0500

    add comment to stack class

--- data-structures/stack.py
+++ data-structures/stack.py
@@ -1,5 +1,6 @@
 class Stack:
     def __init__(self, initial_size = 10):
+        # initialize with array of size 10 - 0s represent 'empty' slots
         self.arr = [0 for _ in range(initial_size)]
         self.next_index = 0
         self.num_elements = 0
