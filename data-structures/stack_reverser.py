68dd426a800b2273e5509381f25865355d09e23f
scirocco21 <sebgertz@gmail.com>
2021-01-11 23:07:53 -0500

    add reverse stack function

---
+++ data-structures/stack_reverser.py
@@ -0,0 +1,57 @@
+"""
+    Reverse a given input stack
+
+    Args:
+       stack(stack): Input stack to be reversed
+    Returns:
+       stack: Reversed Stack
+"""
+
+# SETUP
+class LinkedListNode:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+
+    def __init__(self):
+        self.num_elements = 0
+        self.head = None
+
+    def push(self, data):
+        new_node = LinkedListNode(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.num_elements += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        temp = self.head.data
+        self.head = self.head.next
+        self.num_elements -= 1
+        return temp
+
+    def top(self):
+        if self.head is None:
+            return None
+        return self.head.data
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.num_elements == 0
+
+# MAIN FUNCTION
+def stack_reverse(stack):
+    reverse_stack = Stack()
+    while not stack.is_empty():
+        top = stack.pop()
+        reverse_stack.push(top)
+    return reverse_stack
