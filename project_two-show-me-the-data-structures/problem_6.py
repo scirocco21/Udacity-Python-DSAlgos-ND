a0c56c5cd1641b4cdbcd51cc375cb12c46dd52da
scirocco21 <sebgertz@gmail.com>
2021-02-28 11:20:29 -0500

    stub out intersection method

--- project_two-show-me-the-data-structures/problem_6.py
+++ project_two-show-me-the-data-structures/problem_6.py
@@ -54,6 +54,32 @@ def union(llist_1, llist_2):
       list.copy_values(union)
     return union
 
+def intersection(llist_1, llist_2):
+    list_values_1 = set()
+    list_values_2 = set()
+    intersection = set()
+
+    if llist_1.head is None or llist_2.head is None:
+        return None
+
+    head_1 = llist_1.head
+    head_2 = llist_2.head
+
+    while head_1 or head_2:
+        if head_1:
+            if head_1.value in list_values_2:
+                intersection.add(head_1.value)
+            else:
+                list_values_1.add(head_1.value)
+            head_1 = head_1.next
+        if head_2:
+            if head_2.value in list_values_1:
+                intersection.add(head_2.value)
+            list_values_2.add(head_2.value)
+            head_2 = head_2.next
+
+    return intersection
+
 # Test case 1
 
 linked_list_1 = LinkedList()
@@ -69,6 +95,7 @@ for i in element_2:
     linked_list_2.append(i)
 
 print(union(linked_list_1, linked_list_2))
+print(intersection(linked_list_1, linked_list_2))
 
 # Test case 2
 
@@ -85,3 +112,4 @@ for i in element_2:
     linked_list_4.append(i)
 
 print(union(linked_list_3, linked_list_4))
+print(intersection(linked_list_3, linked_list_4))
