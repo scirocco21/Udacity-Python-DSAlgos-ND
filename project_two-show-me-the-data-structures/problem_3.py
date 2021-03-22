d409074076c6b6cfb8fc51609618edfb8694749b
scirocco21 <sebgertz@gmail.com>
2021-03-22 08:10:29 -0400

    feat: build huffmann tree from priority queue

--- project_two-show-me-the-data-structures/problem_3.py
+++ project_two-show-me-the-data-structures/problem_3.py
@@ -40,43 +40,45 @@ data = "AAAAAAABBBCCCCCCCDDEEEEEE"
 freq_hash = create_freq_hash(data)
 priority_queue = build_min_heap(data)
 
-for item in priority_queue:
-  print(item)
-heapq.heappop(priority_queue)
-print("The smallest item is: ",priority_queue[0])
+# for item in priority_queue:
+#   print(item)
+# heapq.heappop(priority_queue)
+# print("The smallest item is: ",priority_queue[0])
 
-# class HuffmannTree:
-#   def mergeNodes(self,nodeA,nodeB, priority_queue):
-#     print("merging")
-#     node = HuffmannNode()
-#     node.freq = nodeA.freq + nodeB.freq
-#     priority_queue.insert([None, node.freq])
-#     print("ready to return the node ", node.char, node.freq)
-#     return node
+# Step 4: Build HuffmannTree
 
-#   def insert(self,priority_queue):
-#     print("Inserting into tree: ")
-#     nodeA = priority_queue.getMin()
-#     priority_queue.delete_min()
-#     nodeB = priority_queue.getMin()
-#     priority_queue.delete_min()
+class HuffmannTree:
+  def mergeNodes(self,nodeA,nodeB, priority_queue):
+    print("merging")
+    node = HuffmannNode()
+    node.freq = nodeA.freq + nodeB.freq
+    heapq.heappush(priority_queue,[node.freq,None])
+    print("ready to return the node ", node.char, node.freq)
+    return node
 
-#     new_node_A = HuffmannNode(nodeA)
-#     new_node_B = HuffmannNode(nodeB)
-#     new_node_C = self.mergeNodes(new_node_A, new_node_B, priority_queue)
-#     new_node_C.left = new_node_A 
-#     new_node_C.right = new_node_B
-#     print("nodeA: ", new_node_A.char,new_node_A.freq)
-#     print("nodeB:", new_node_B.char, new_node_B.freq)
-#     print("nodeC:", new_node_C.freq)
-# class HuffmannNode:
-#   def __init__(self,node=None):
-#     self.right = None
-#     self.left = None
-#     self.freq = node[1] if node else None
-#     self.char = node[0] if node else None
+  def insert(self,priority_queue):
+    print("Inserting into tree: ")
+    nodeA = priority_queue[0]
+    heapq.heappop(priority_queue)
+    nodeB = priority_queue[0]
+    heapq.heappop(priority_queue)
 
-# hufftree = HuffmannTree()
+    new_node_A = HuffmannNode(nodeA)
+    new_node_B = HuffmannNode(nodeB)
+    new_node_C = self.mergeNodes(new_node_A, new_node_B, priority_queue)
+    new_node_C.left = new_node_A 
+    new_node_C.right = new_node_B
+    print("nodeA: ", new_node_A.char,new_node_A.freq)
+    print("nodeB:", new_node_B.char, new_node_B.freq)
+    print("nodeC:", new_node_C.freq)
+class HuffmannNode:
+  def __init__(self,node=None):
+    self.right = None
+    self.left = None
+    self.freq = node[0] if node else None
+    self.char = node[1] if node else None
 
-# while len(priority_queue.heap) >= 1:
-#   hufftree.insert(priority_queue)
+hufftree = HuffmannTree()
+
+while len(priority_queue) > 1:
+  hufftree.insert(priority_queue)
